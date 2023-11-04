0238f6032af87f73d686b43aef3f16f117453b31
Rafal Macyszyn <chommik12@gmail.com>
2023-11-04 17:31:34 +0100

    fixes

--- inc/customizer.php
+++ inc/customizer.php
@@ -56,6 +56,6 @@ function v92v2_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function v92v2_customize_preview_js() {
-	wp_enqueue_script( 'v92v2-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+	wp_enqueue_script( 'v92v2-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), V92V2_VERSION, true );
 }
 add_action( 'customize_preview_init', 'v92v2_customize_preview_js' );
